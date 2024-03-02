1c794a8401565b4239e2734e12de708748858693
karivas-dev <karenrivasg17@gmail.com>
2024-03-02 17:17:50 -0600

    Create new course

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,11 @@
+import controllers.CourseController
+import models.Course
 import services.Menu
 
 fun main() {
     //Menu.addOption("Test", TestController::test)
+
+    Menu.addOption("Agregar un curso nuevo", CourseController::create)
 
     runApp()
 }
---
+++ src/controllers/CourseController.kt
@@ -0,0 +1,35 @@
+package controllers
+
+import models.Course
+import models.Student
+import stores.CourseStore
+import stores.StudentStore
+
+object CourseController {
+    fun create() {
+        println("CREACIÓN DE UN CURSO NUEVO")
+        println("Ingrese el nombre del curso:")
+        val courseName = readln()
+        println("Ingrese los ids de los estudiantes que pertenecerán al curso, separados por comas:")
+        val studentsIds = readln().split(",").map { it.trim() }
+        val verifiedStudents: MutableList<String> = mutableListOf()
+
+        studentsIds.forEach {
+            if (it !in StudentStore.students.map { it.id.toString() }) {
+                println("El id $it no corresponde a ningún estudiante.")
+            } else {
+                studentsIds.drop(studentsIds.indexOf(it))
+                verifiedStudents.addFirst(it)
+            }
+        }
+
+        if (verifiedStudents.isNotEmpty()) {
+            CourseStore.add(Course(courseName, verifiedStudents))
+            println("Curso creado con éxito y alumnos añadidos correctamente.")
+        } else {
+            CourseStore.add(Course(courseName, verifiedStudents))
+            println("Curso creado con éxito y sin añadir alumnos.")
+        }
+        readln()
+    }
+}
