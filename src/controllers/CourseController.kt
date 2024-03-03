e7c99a8326db60ca95a3fc9dd37bb60d3111b812
karivas-dev <karenrivasg17@gmail.com>
2024-03-02 19:17:52 -0600

    Show one course details

--- src/controllers/CourseController.kt
+++ src/controllers/CourseController.kt
@@ -15,7 +15,22 @@ object CourseController {
         readln()
     }
 
+    fun show() {
+        println("MOSTRAR UN CURSO")
+        println("Ingrese el código del curso que desea mostrar:")
+        val courseId = readln()
 
+        CourseStore.courses.find { it.id == courseId.toInt()}?.let { course ->
+            println("Código\tNombre\tAlumnos inscritos")
+            println("${course.id}\t\t${course.name}\t\t${course.students.count()}")
+            println("ID\tAlumnos inscritos")
+            course.students.forEach {
+                println("${it.id}\t${it.name}")
+            }
+        } ?: println("El curso no existe en el registro.")
+
+        readln()
+    }
 
     fun create() {
         println("CREACIÓN DE UN CURSO NUEVO")
